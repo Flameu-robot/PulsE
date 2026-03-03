c63e745bdac935118aff68fc962059b06ef7a9a6
Flameu-robot <nicksh.04@mail.ru>
2026-03-03 19:32:10 +0700

    настройка ci.yml v4

--- backend/identity-service/settings.gradle.kts
+++ backend/identity-service/settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "identity-service"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -14,9 +14,4 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
-    versionCatalogs {
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
-    }
 }
