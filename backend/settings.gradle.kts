a402a3886ec995aaf374ccfca37fdc0ada68fdc2
Flameu-robot <nicksh.04@mail.ru>
2026-03-03 19:20:46 +0700

    настройка ci.yml v3

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -14,4 +14,9 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
 }
