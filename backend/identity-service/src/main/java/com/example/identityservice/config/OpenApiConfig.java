61ff54d8248b29744685ada7b6b382f8e6014f31
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2026-03-06 19:04:39 +0700

    feat: 1. add service register in gateway       2. sync gateway + identity services       3. add openapi server config

--- backend/identity-service/src/main/java/com/example/identityservice/config/OpenApiConfig.java
+++ backend/identity-service/src/main/java/com/example/identityservice/config/OpenApiConfig.java
@@ -5,11 +5,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class OpenApiConfig {
+
+    @Value("${app.gateway.url:http://localhost:8000}")
+    private String gatewayUrl;
 
     @Bean
     public OpenAPI openAPI() {
@@ -19,6 +26,12 @@ public class OpenApiConfig {
                         .version("1.0")
                         .description("Authentication, authorization and user management")
                 )
+
+                .servers(List.of(
+                        new Server()
+                                .url(gatewayUrl)
+                                .description("Gateway Server")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer"))
                 .components(new Components()
                         .addSecuritySchemes("Bearer", new SecurityScheme()
